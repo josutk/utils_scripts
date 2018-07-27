6d9a39a458ca21ee261edc41489d940377a46536
josutk <josuetk63@gmail.com>
2018-07-27 16:28:56 -0300

    Update poco script

--- poco.bat
+++ poco.bat
@@ -1,37 +1,28 @@
 @ECHO OFF
 set /p type_build=build or rebuild:
+
 if "%type_build%"=="rebuild" goto rrebuild
 	echo "Start Build"
-	cd CppUnit
-	msbuild /t:Build CppUnit_vs120.sln 
+	timeout 3 > nul
+	msbuild /t:Build CppUnit/CppUnit_vs120.sln
+	msbuild /t:Build Foundation/Foundation_vs120.sln 
+	msbuild /t:Build XML/XML_vs120.sln
+	msbuild /t:Build JSON/JSON_vs120.sln 
+	msbuild /t:Build Util/Util_vs120.sln 
+	msbuild /t:Build Net/Net_vs120.sln	
+	echo "Finish build poco"
 	cd ..
-	cd Foundation
-	msbuild /t:Build Foundation_vs120.sln 
-	cd ..
-	cd Util 
-	msbuild /t:Build Util_vs120.sln 
-	cd ..
-	cd XML 
-	msbuild /t:Build XML_vs120.sln 
-	cd ..
-	cd Net
-	msbuild /t:Build Net_vs120.sln	
 goto end
 
 :rrebuild
 	echo "Start Rebuild"
-	cd CppUnit
-	msbuild /t:Rebuild CppUnit_vs120.sln 
+	timeout 3 > nul
+	msbuild /t:Rebuild CppUnit/CppUnit_vs120.sln
+	msbuild /t:Rebuild Foundation/Foundation_vs120.sln 
+	msbuild /t:Rebuild XML/XML_vs120.sln
+	msbuild /t:Rebuild JSON/JSON_vs120.sln 
+	msbuild /t:Rebuild Util/Util_vs120.sln 
+	msbuild /t:Rebuild Net/Net_vs120.sln	
+	echo "Finish rebuild poco"
 	cd ..
-	cd Foundation
-	msbuild /t:Rebuild Foundation_vs120.sln 
-	cd ..
-	cd Util 
-	msbuild /t:Rebuild Util_vs120.sln 
-	cd ..
-	cd XML 
-	msbuild /t:Rebuild XML_vs120.sln 
-	cd ..
-	cd Net
-	msbuild /t:Rebuild Net_vs120.sln	
 :end
